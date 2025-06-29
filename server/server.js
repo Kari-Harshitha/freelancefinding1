150fda05ef19ec91b41f3a56b0865c8d01996011
Kari-Harshitha <kariharshitha22@gmail.com>
2025-06-29 11:48:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const projectRoutes = require('./routes/projects');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/sbworks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use('/api/projects', projectRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
